54cfa76efb462988fd1b8ecbd95fcc4f23c3b9ab
yeonblue <yeondublue@naver.com>
2023-02-13 11:29:54 +0900

    complete feature_1

--- CiCdExample/ViewController.swift
+++ CiCdExample/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // MARK: - IBActions
+    @IBAction func helloButtonTapped(_ sender: Any) {
+        nameLabel.text = nameTextfield.text
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
